c03f2473b222f8aac22d6f4251fc7c4551baaa3b
Gnadi <johannes.gnadlinger1997@gmail.com>
2017-11-12 11:28:50 +0100

    added generic compare method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,10 +11,22 @@ public class Main {
         //Was ist aber wenn man zu diesem Zeitpunkt noch gar nicht weiß welchen Datentyp der Parameter hat?
         OrderedPair<String, ProductId<Integer>> p4 = new OrderedPair<>("Puma", new ProductId<Integer>(1));
 
+        //Ausgabe der Werte
         System.out.println("P1: "+p1.getValue());
         System.out.println("P2: "+p2.getValue());
         System.out.println("P3: "+p3.getValue());
         System.out.println("P4: "+p4.getValue().get());
+
+        //Generic Method to compare two pairs
+        OrderedPair<String, String> pc = new OrderedPair<String, String>("Airmax", "1000df382gS?8");
+        OrderedPair<String, String> pc1 = new OrderedPair<>("Airmax", "1000df382gS?8");
+        boolean same = Util.compare(pc,pc1);
+        System.out.println(same);
+
+        OrderedPair<String, Integer> pc2 = new OrderedPair<String, Integer>("Nike", 10373);
+        OrderedPair<String, Integer> pc3 = new OrderedPair<>("Airmax", 234);
+        boolean notsame = Util.<String, Integer>compare(pc2, pc3);
+        System.out.println(notsame);
 
 
     }
